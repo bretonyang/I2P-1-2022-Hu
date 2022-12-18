78ebadc9aeffcad47f2a3b2ad8647228f1b3824a
bretonyang <breton0429@gmail.com>
2022-12-19 01:00:53 +0800

    Added HW10

---
+++ HW/HW10/13751_Super_Idol/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int start, end;
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        scanf("%u %u", &start, &end);
+
+        // If two numbers cross the boundary of 2^n,
+        // i.e. start < 2^n < end, this means there is no common 1s
+        // exist in all the number. so the real question is to
+        // find the left-most common 1s, if any.
+        unsigned int result = 0;
+        while (start != end) {
+            start = start >> 1; // modified destructively
+            end = end >> 1; // modified destructively
+            result++;
+        }
+
+        printf("%u\n", start << result); // NOTE: start is modified
+    }
+
+    return 0;
+}
+
+/*
+
+ 1100101  01000 -> start
+ 1100101  ...
+ 1100101  10101 -> end
+
+ 1100101  00000 -> answer
+
+ after while loop, `start`: 1100101, `result`: 5
+ `start << result`: 1100101 00000
+
+
+*/
+
+
+
+
+
+
+
+
+
+
